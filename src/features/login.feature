19db94d7073bed86a660c7db9fb9d4e7886239d5
Saqib Hussain <saqib1397@gmail.com>
2023-08-09 23:22:12 +0500

    cucumber and playwright project basic level setup is done

---
+++ src/features/login.feature
@@ -0,0 +1,5 @@
+Feature: Example Feature
+  Scenario: Opening a website
+    Given I open the website
+    When I do something
+    Then I should see something
---
+++ src/features/step_definitions/login.steps.js
@@ -0,0 +1,18 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { chromium } = require('playwright');
+
+         Given('I open the website', function () {
+           // Write code here that turns the phrase above into concrete actions
+           console.log('testing')
+         });
+
+
+         When('I do something', function () {
+           // Write code here that turns the phrase above into concrete actions
+           console.log('testing')
+         });
+
+         Then('I should see something', function () {
+           // Write code here that turns the phrase above into concrete actions
+           console.log('testing')
+         });
